82eacc997b2bd8fb675476e6c657c2bde3d84525
Joyce <yeechiajoyce@gmail.com>
2021-05-14 17:38:03 +0800

    vuex、vue-router初始設定

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import store from '@src/assets/js/store';
+import router from '@src/assets/js/router';
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
+  router,
   render: h => h(App),
 }).$mount('#app')
